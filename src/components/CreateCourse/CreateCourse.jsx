4c5ebe812b6470d7b9a3262b87c2ea9b1e6b0ffb
FercanSen <fercansen@gmail.com>
2022-11-01 18:38:46 +0300

    Add and delete author

--- src/components/CreateCourse/CreateCourse.jsx
+++ src/components/CreateCourse/CreateCourse.jsx
@@ -1,37 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../../common/Button/Button";
+import { mockedAuthorsList } from "../../constants";
+import AuthorItem from "./components/AuthorItem/AuthorItem";
 
 function CreateCourse() {
+  const [authorInput, setAuthorInput] = useState("");
+  const [courseAuthors, setCourseAuthors] = useState([]);
+  const [author, setAuthor] = useState("");
+  let authorId = Math.random();
+
+  useEffect(() => {
+    if (author) {
+      mockedAuthorsList.push({ id: authorId, name: author });
+    }
+  }, [author]);
+
+  const handleChange = (event) => {
+    setAuthorInput(event.target.value);
+  };
+
+  const handleClick = () => {
+    setAuthor(authorInput);
+    setAuthorInput("");
+  };
+
+  function addAuthor(authorName) {
+    setCourseAuthors([...courseAuthors, authorName]);
+  }
+
+  function deleteAuthor(authorName) {
+    // setCourseAuthors([...courseAuthors, authorName]);
+    setCourseAuthors(courseAuthors.filter((item) => item !== authorName));
+  }
+
   return (
-    <div className="flex flex-col bg-blue-200">
-      <div className="flex justify-between items-center">
-        <div className="flex-col bg-red-400">
-          <h2>Title</h2>
-          <input type="text" placeholder="Enter Title..." name="title" />
+    <div className="flex flex-col m-8 p-4 border-2 rounded">
+      <div className="flex  justify-between items-center">
+        <div className="flex-col">
+          <h2 className="my-2">Title</h2>
+          <input
+            className="border-2 border-blue-500 rounded"
+            type="text"
+            placeholder="Enter Title..."
+            name="title"
+          />
           <label htmlFor="title"></label>
         </div>
         <Button buttonText={"Create course"} />
       </div>
-      <h2>Description</h2>
+      <h2 className="my-2">Description</h2>
       <textarea
         className="h-36 border-2 border-blue-500 rounded"
-        value={"Enter Description"}
+        placeholder="Enter Description"
+        minLength={2}
       />
-      <div className="flex justify-evenly bg-green-500">
+      <div className="flex justify-evenly ">
         <h2>Add author</h2>
         <h2>Authors</h2>
       </div>
-      <div className="flex ">
-        <div className="w-1/2 bg-red-500">
-          <h2>Author name</h2>
-          <input type="text" placeholder="Enter Title..." name="title" />
-          <label htmlFor="title"></label>
+      <div className="flex">
+        <div className="flex flex-col w-1/2 p-2 ">
+          <h2 className="my-2">Author name</h2>
+          <input
+            className="border-2 border-blue-500 rounded"
+            type="text"
+            placeholder="Enter author name..."
+            name="authorName"
+            minLength={2}
+            value={authorInput}
+            onChange={handleChange}
+          />
+          <label htmlFor="authorName"></label>
+          <div className="flex flex-row justify-center my-2">
+            <Button buttonText={"Create author"} onClick={handleClick} />
+          </div>
         </div>
-        <div className="w-1/2 bg-yellow-500">Author List</div>
+        <div className="w-1/2 ">
+          {mockedAuthorsList.map((element, index) => (
+            <AuthorItem
+              key={index}
+              authorName={element.name}
+              btnText="Add author"
+              btnOnClick={() => addAuthor(element.name)}
+            />
+          ))}
+        </div>
       </div>
-      <div className="flex justify-evenly bg-green-500">
+      <div className="flex justify-evenly ">
         <h2>Duration</h2>
         <h2>Course authors</h2>
+      </div>
+      <div className="flex justify-evenly">
+        <div className="flex flex-col w-1/2 p-2 ">
+          <h2 className="my-2">Duration</h2>
+          <input
+            className="border-2 border-blue-500 rounded"
+            type="text"
+            placeholder="Enter duration in minutes..."
+            name="duration"
+          />
+          <label htmlFor="duration"></label>
+          <h2 className="my-2">Duration: 00:00 hours</h2>
+        </div>
+        {/* <div>{courseAuthors}</div> */}
+
+        <div className="w-1/2">
+          {courseAuthors.map((element, index) => (
+            <AuthorItem
+              key={index}
+              authorName={element}
+              btnOnClick={() => deleteAuthor(element)}
+              btnText="Delete author"
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/CreateCourse/components/AuthorItem/AuthorItem.jsx
+++ src/components/CreateCourse/components/AuthorItem/AuthorItem.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Button from "../../../../common/Button/Button";
+import PropTypes from "prop-types";
 
-function AuthorItem() {
+function AuthorItem({ authorName, btnText, btnOnClick }) {
+  AuthorItem.propTypes = {
+    authorName: PropTypes.string.isRequired,
+    btnText: PropTypes.string.isRequired,
+    btnOnClick: PropTypes.func,
+  };
+
   return (
-    <div>
-      <p>Author Name</p>
-      <Button />
+    <div className="flex justify-between items-center m-2">
+      <p>{authorName}</p>
+      <Button buttonText={btnText} onClick={btnOnClick} />
     </div>
   );
 }
