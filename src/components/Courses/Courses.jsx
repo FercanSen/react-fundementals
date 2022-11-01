945f4666fc24d834a36192bbebffbc836b4aae4b
FercanSen <fercansen@gmail.com>
2022-11-01 18:39:10 +0300

    Add conditional rendering

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -4,13 +4,15 @@ import CourseCard from "./components/CourseCard/CourseCard";
 import SearchBar from "./components/SearchBar/SearchBar";
 import PropTypes from "prop-types";
 import getCourseDuration from "../../helpers/getCourseDuration";
+import CreateCourse from "../CreateCourse/CreateCourse";
 
 export default function Courses({ mockedCoursesList }) {
   Courses.propTypes = {
     mockedCoursesList: PropTypes.array,
   };
-
   const [query, setQuery] = useState("");
+  const [showCreateCourseFrom, setShowCreateCourseFrom] = useState(false);
+
   function handleChange(event) {
     setQuery(event.target.value);
   }
@@ -19,8 +21,12 @@ export default function Courses({ mockedCoursesList }) {
     <div className="m-8">
       <div className="flex justify-between">
         <SearchBar onChange={handleChange} />
-        <Button buttonText={"Add new course"} />
+        <Button
+          buttonText={"Add new course"}
+          onClick={() => setShowCreateCourseFrom(!showCreateCourseFrom)}
+        />
       </div>
+      {showCreateCourseFrom && <CreateCourse />}
       {mockedCoursesList
         .filter((course) => course.title.toLocaleLowerCase().includes(query))
         .map((element, index) => {
