a9e46aff3619f28fe711def5d2349d69ae63afc9
FercanSen <fercansen@gmail.com>
2022-11-02 17:30:17 +0300

    Add format creation date

--- src/helpers/formatCreationDate.js
+++ src/helpers/formatCreationDate.js
@@ -1,1 +1,9 @@
- 
+function formatCreationDate() {
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
+  return date;
+}
+
+export default formatCreationDate;
