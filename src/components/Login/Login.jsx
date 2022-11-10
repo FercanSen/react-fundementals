84a3657b7b3988a45efaa15102d5ec8ebb270205
FercanSen <fercansen@gmail.com>
2022-11-10 15:43:48 +0300

    Add login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../common/Button/Button";
 import Input from "../../common/Input/Input";
+import axios from "axios";
 
 function Login() {
+  const navigate = useNavigate();
+  
+  function login(event) {
+    event.preventDefault();
+    axios
+      .post("http://localhost:4000/login", {
+        name: event.target.elements.name.value,
+        email: event.target.elements.email.value,
+        password: event.target.elements.password.value,
+      })
+      .then(function (response) {
+        console.log(response.data.result);
+        localStorage["userToken"] = response.data.result;
+        navigate("/courses");
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log(error.response);
+        console.log(error.message);
+        console.log(error.request);
+      });
+  }
+
   return (
-    <form action="">
+    <form action="" onSubmit={login}>
       <div className="flex flex-col gap-7 items-center justify-center h-screen">
-        <Input labelText={"Email"} placeholderText={"Enter email"} />
-        <Input labelText={"Password"} placeholderText={"Enter password"} />
+        <Input
+          labelText={"Name"}
+          placeholderText={"Enter name"}
+          inputName={"name"}
+        />
+        <Input
+          labelText={"Email"}
+          placeholderText={"Enter email"}
+          inputName={"email"}
+        />
+        <Input
+          labelText={"Password"}
+          placeholderText={"Enter password"}
+          inputName={"password"}
+        />
         <Button buttonText={"Login"} />
         <div>
           If you do not have an account{" "}
