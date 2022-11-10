f56f521ccb8bf2a0162d670e2c1fc780f64165c8
FercanSen <fercansen@gmail.com>
2022-11-10 15:43:24 +0300

    Add registration page

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -4,41 +4,45 @@ import Input from "../../common/Input/Input";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-
-
 function Registration() {
-    function submitRegister(event) {
-      axios
-        .post("http://localhost:4000/register", {
-          name: event.target.elements.name.value,
-          email: event.target.elements.email.value,
-          password: event.target.elements.password.value,
-        })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error.response);
-        });
-        event.preventDefault();
-    }
+  function submitRegister(event) {
+    axios
+      .post("http://localhost:4000/register", {
+        name: event.target.elements.name.value,
+        email: event.target.elements.email.value,
+        password: event.target.elements.password.value,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error.response);
+      });
+    event.preventDefault();
+  }
 
-//   function handleRegister(event) {
-//     console.log(event.target.elements.name.value);
-//     console.log(event.target.elements.email.value);
-//     console.log(event.target.elements.password.value);
-//     event.preventDefault();
-//   }
   return (
     <form action="" onSubmit={submitRegister}>
       <div className="flex flex-col gap-7 items-center justify-center h-screen">
-        <Input labelText={"Name"} placeholderText={"Enter name"} inputName={"name"} />
-        <Input labelText={"Email"} placeholderText={"Enter email"} inputName={"email"}/>
-        <Input labelText={"Password"} placeholderText={"Enter password"} inputName={"password"}/>
+        <Input
+          labelText={"Name"}
+          placeholderText={"Enter name"}
+          inputName={"name"}
+        />
+        <Input
+          labelText={"Email"}
+          placeholderText={"Enter email"}
+          inputName={"email"}
+        />
+        <Input
+          labelText={"Password"}
+          placeholderText={"Enter password"}
+          inputName={"password"}
+        />
         <Button buttonText={"Registration"} />
         <div>
           If you have an account{" "}
-          <Link className="text-blue-500" to={`/Login`}>
+          <Link className="text-blue-500" to={`/login`}>
             Login
           </Link>
         </div>
