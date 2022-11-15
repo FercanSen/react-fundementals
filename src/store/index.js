951243416292246b6eb4931d183e005a904074f7
FercanSen <fercansen@gmail.com>
2022-11-15 06:38:39 +0300

    Remove mock data

--- src/store/index.js
+++ src/store/index.js
@@ -8,23 +8,7 @@ export const initialState = {
     token: "", // default value - empty string or token value from localStorage. After success login - value from API /login response.
   },
   // todo: initialize this courses to an empty array, delete mock data
-  courses: [
-    {
-      id: "de5aaa59-90f5-4dbc-b8a9-aaf205c551ba",
-      title: "JavaScript",
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-                  has been the industry's standard dummy text ever since the 1500s, when an unknown 
-                  printer took a galley of type and scrambled it to make a type specimen book. It has survived 
-                  not only five centuries, but also the leap into electronic typesetting, remaining essentially u
-                  nchanged.`,
-      creationDate: "08/03/2021",
-      duration: 160,
-      authors: [
-        "27cc3006-e93a-4748-8ca8-73d06aa93b6d",
-        "f762978b-61eb-4096-812b-ebde22838167",
-      ],
-    },
-  ], // list of courses. Default value - empty array. After success getting courses - value from API /courses/all response.
+  courses: [], // list of courses. Default value - empty array. After success getting courses - value from API /courses/all response.
   // todo: initialize this authors to empty an array, delete mock data
   authors: [], //  list of authors. Default value - empty array. After success getting authors - value from API /authors/all response.
 };
@@ -37,7 +21,7 @@ const courses = createSlice({
       state.courses = [action.payload, ...state.courses];
     },
     addAuthor: (state, action) => {
-      state.authors = [action.payload, ...state.authors];
+      state.authors = action.payload;
     },
     deleteCourse: (state, action) => {
       const indexOfObject = state.courses.findIndex((object) => {
@@ -46,7 +30,7 @@ const courses = createSlice({
       state.courses.splice(indexOfObject, 1);
     },
     createAuthor: (state, action) => {
-      state.authors[0] = [action.payload, ...state.authors[0]];
+      state.authors = [action.payload, ...state.authors];
     },
     saveUser: (state, action) => {
       state.user = {
